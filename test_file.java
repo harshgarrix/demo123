f044f088ee27965707958451d23b5efd04bfe221
harshgarrix <hlakhlotra@gmail.com>
2024-06-20 11:52:24 +0530

    initialize

---
+++ test_file.java
@@ -0,0 +1,10 @@
+package docker_test;
+
+public class test_file {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello this is a test file for docker.");
+	}
+
+}
